9bbe6622fb3fe09b820d81ef559b00410b40b3a9
Chuyang Zhou <469107073@qq.com>
2021-07-26 14:51:08 +1000

    Update

---
+++ emacs/doom.d/config.el
@@ -0,0 +1,88 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Chuyang Zhou"
+      user-mail-address "469107073@qq.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Monaco" :size 18)
+      doom-variable-pitch-font (font-spec :family "Monaco" :size 18))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Developer/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; LSP for cc mode, clangd config
+;;(setq lsp-clients-clangd-args '("-j=3"
+;;                                "--background-index"
+;;                                "--clang-tidy"
+;;                                "--completion-style=detailed"
+;;                                "--header-insertion=never"
+;;                                "--header-insertion-decorators=0"))
+;;(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
+(setq lsp-auto-guess-root nil)
+
+;; Set conda base :conda info --base to show
+(setq conda-env-autoactivate-mode t)
+(custom-set-variables
+ '(conda-anaconda-home "/Users/chuyangzhou/miniforge3"))
+
+
+;; Key Chord plugin to map jj to ESC
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
+
+;; Evil mode keybindings
+(define-key evil-normal-state-map (kbd "L") 'evil-end-of-line)
+(define-key evil-normal-state-map (kbd "H") 'evil-beginning-of-line)
+
+;; LSP settings
+(setq lsp-headerline-breadcrumb-enable t)
+
+;; Treemacs project sidebar
+(setq treemacs-width 25)
+(require 'treemacs-all-the-icons)
+(setq doom-themes-treemacs-theme "all-the-icons")
