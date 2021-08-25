896c46e04639c6a0b4b2323418700751477f540c
Chuyang Zhou <469107073@qq.com>
2021-08-25 16:29:33 +1000

    Update

--- st/config.h
+++ st/config.h
@@ -165,7 +165,7 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 120;
+static unsigned int cols = 100;
 static unsigned int rows = 40;
 
 /*
