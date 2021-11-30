ea68fb7982ffaf159a62fe04dbe5af56ca1a80ff
Chuyang Zhou <45252429+zcyzhou@users.noreply.github.com>
2021-11-30 15:18:34 +1100

    Update my-nvimtree.lua

--- nvim/lua/my-nvimtree.lua
+++ nvim/lua/my-nvimtree.lua
@@ -9,12 +9,12 @@ require'nvim-tree'.setup {
 	nvim_tree_update_cwd = 1,
 	nvim_tree_follow = 1,
 	nvim_tree_hide_dotfiles = 1,
-	nvim_tree_ignore = {".git", "node_modules", ".cache"}
+	nvim_tree_ignore = {".git", "node_modules", ".cache"},
+	nvim_tree_gitignore = 1
 }
 
 g.nvim_tree_side = "left"
 g.nvim_tree_width = 25
-g.nvim_tree_gitignore = 1
 g.nvim_tree_auto_ignore_ft = {"dashboard"} -- don't open tree on specific fiypes.
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_indent_markers = 1
